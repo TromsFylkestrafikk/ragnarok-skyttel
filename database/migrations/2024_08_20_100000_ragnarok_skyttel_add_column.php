b2377a1365233ad7da711e5449efdb5b397cff67
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2024-08-20 10:44:06 +0200

    fix: Re-add column IssuerIDCharged

---
+++ database/migrations/2024_08_20_100000_ragnarok_skyttel_add_column.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('skyttel_transaction_receipts', function (Blueprint $table) {
+            $table->char('IssuerIDCharged')->nullable()->after('ChargedNetAmountToll')->comment('ID of issuer. 6 digit hex value, often `999999` or null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
